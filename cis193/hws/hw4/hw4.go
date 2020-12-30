e109c419f8db9d83c29202a39b36c8826b06811c
Calvin D. Woo <calvin.d.woo@gmail.com>
2020-12-30 04:17:19 -0500

    worked on concurrency hw4?

--- cis193/hws/hw4/hw4.go
+++ cis193/hws/hw4/hw4.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"log"
+	"os"
+	"strconv"
+	"sync"
 )
 
 func main() {
-
+	FileSum("file_sum.txt", "file_res.txt")
 }
 
 // Problem 1a: File processing
@@ -18,7 +24,27 @@ func main() {
 // You should expect your input to end with a newline, and the output should
 // have a newline after the result.
 func FileSum(input, output string) {
-	// TODO
+	fi, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var res int = 0
+
+	// scan file
+	scanner := bufio.NewScanner(fi)
+	for scanner.Scan() {
+		i, _ := strconv.Atoi(scanner.Text())
+		res += i
+	}
+
+	fo, err := os.Create(output)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fo.WriteString(fmt.Sprint(res))
+	defer fo.Close()
 }
 
 // Problem 1b: IO processing with interfaces
@@ -48,8 +74,8 @@ func IOSum(input io.Reader, output io.Writer) {
 // You may only add *private* fields to this struct.
 // Hint: Use an embedded sync.RWMutex, see lecture 2 for a review on embedding
 type PennDirectory struct {
-	// TODO
 	directory map[int]string
+	mu        sync.RWMutex
 }
 
 // Add inserts a new student to the Penn Directory.
@@ -57,20 +83,31 @@ type PennDirectory struct {
 // writes to the map.
 // You may NOT write over existing data - simply raise a warning.
 func (d *PennDirectory) Add(id int, name string) {
-	// TODO
+	_, ok := d.directory[id]
+	if !ok {
+		d.mu.Lock()
+		d.directory[id] = name
+		d.mu.Unlock()
+	} else {
+		fmt.Println("Can't override a value!")
+	}
 }
 
 // Get fetches a student from the Penn Directory by their PennID.
 // Get should obtain a read lock, and should allow concurrent read access but
 // not write access.
 func (d *PennDirectory) Get(id int) string {
-	// TODO
-	return ""
+	d.mu.RLock()
+	v := d.directory[id]
+	d.mu.RUnlock()
+	return v
 }
 
 // Remove deletes a student to the Penn Directory.
 // Remove should obtain a write lock, and should not allow any concurrent reads
 // or writes to the map.
 func (d *PennDirectory) Remove(id int) {
-	// TODO
+	d.mu.Lock()
+	delete(d.directory, id)
+	d.mu.Unlock()
 }
